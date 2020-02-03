616a83cf5b26b1fe9fb9d09da57b50d5081c5f0d
Abhishek Mahajan <abhishek0000mahajan@gmail.com>
2020-02-03 16:07:27 +0530

    Added more functions.

--- script.js
+++ script.js
@@ -3,40 +3,46 @@ function computerPlay(){
     let choice = choices[Math.floor((Math.random()*3))];
     return choice;
 }
+
 function playRound(computerSelection, playerSelection){
-    playerSelection=playerSelection.toLowerCase();
-    switch(computerSelection){
-        case "paper":
-           if(playerSelection==="scissors") return "win";
-           else if(playerSelection==="paper")return "draw";
-           else return "loss";
-           break;
-        case "scissors":
-            if(playerSelection==="scissors") return "draw";
-           else if(playerSelection==="paper")return "loss";
-           else return "win";
-           break;
-        case "rock":
-            if(playerSelection==="scissors") return "loss";
-           else if(playerSelection==="paper")return "win";
-           else return "draw";
-           break;
-    }
-}
-function game(){
-    let count=1, playerWins=0, computerWins=0;
-    while(count<=5){
-        playerSelection = window.prompt("Enter rock, paper or scissors!");
-        if(/^(rock|paper|scissors)$/i.test(playerSelection)){
-            let result = playRound(computerPlay(),playerSelection);
-            if(result=="win")playerWins++;
-            else if(result=="loss")computerWins++;
-            console.log("Round "+count+" : "+result);
-            count++;
+        if((playerSelection==="rock"&&computerSelection==="scissors")
+                    ||(playerSelection==="paper"&&computerSelection=="rock")
+                    ||(playerSelection==="scissors"&&computerSelection==="paper")){
+            return "win";
         }
-        else alert("Wrong input!");
-    }
-    console.log("Score = "+playerWins+":"+computerWins);
-    console.log((playerWins>computerWins) ? "You win." : "Computer wins.");
+        else if(playerSelection==="rock"&&computerSelection==="paper"
+                    ||playerSelection==="paper"&&computerSelection==="scissors"
+                    ||playerSelection==="scissors"&&computerSelection==="rock"){
+            return "loss";
+       }
+       else return "draw";
 }
+
+function playerInput(){
+    while(true){
+        let playerSelection = window.prompt("Enter rock, paper, or scissors!");
+        if(/^(rock|paper|scissors)$/i.test(playerSelection)){
+            return playerSelection;
+        }
+        else alert("Wrong input! Enter again.");
+    }
+}
+
+function game(){
+    let count=1, playerWins=0, computerWins=0; var result;
+    while(count<=5){
+        playerSelection = playerInput();
+        result = playRound(computerPlay(),playerSelection);
+        if(result=="loss")computerWins++;
+        else if(result=="win")playerWins++;
+        console.log(`Round ${count} : ${result}`);
+        count++;
+    }
+    console.log(`Final Score => Computer ${computerWins} : ${playerWins} Player`);
+    result = (playerWins > computerWins)?"You win!"
+            :(playerWins == computerWins)?"It's a tie!"
+            :"You lose.";
+    console.log(result);
+}
+
 game();
